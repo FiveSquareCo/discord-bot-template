cd61d6a781685416251d459ef8f5269f7aaf2ab2
Parthasarathi-EXT1NT <dev.parthasarathi1@gmail.com>
2021-02-14 17:18:41 +0530

    Upgraded leveling system

--- commands/leveling/level.js
+++ commands/leveling/level.js
@@ -1,6 +1,8 @@
 const levelSchema = require("../../models/levelSchema");
 const Discord = require("discord.js");
 const canvacord = require("canvacord");
+const { embedColor } = require("../../configs/config.json");
+const { levelCommandMessage } = require("../../configs/levels.json");
 const { join } = require("path");
 module.exports = {
   name: "level",
@@ -8,12 +10,14 @@ module.exports = {
   category: "leveling",
   description: "Level of a User",
   callback: async ({ message }) => {
+    const author = message.author;
     const guildId = message.guild.id;
     const userId = message.member.id;
     const image = join(__dirname, "..", "..", "images", "welcomeBG.png");
+
     // console.log(image);
 
-    await levelSchema.findOne({ guildId, userId }, (err, result) => {
+    await levelSchema.findOne({ guildId, userId }, async (err, result) => {
       if (!result) {
         message.reply(
           `You Dont have Any Level, First Send Some Mesages then Check Back!`
@@ -21,27 +25,65 @@ module.exports = {
         return;
       }
       const { xp, level } = result;
+      const results = await levelSchema
+        .find({
+          guildId,
+        })
+        .sort({
+          level: -1,
+          xp: -1,
+        });
+      let userRank;
+      for (var i = 0; i < results.length; i++) {
+        if (results[i].userId === userId) {
+          userRank = i + 1;
+        }
+      }
+      if (levelCommandMessage.toLocaleLowerCase() === "embed") {
+        const name = `${author.username}#${author.discriminator}`;
+        const levelEmbed = new Discord.MessageEmbed()
+          .setAuthor(name, author.displayAvatarURL({ dynamic: true }))
+          .setTitle("Level/Rank")
+          .setThumbnail(author.displayAvatarURL({ dynamic: true }))
+          .setDescription(
+            `
+  :dizzy: Exp : ${xp}
+  :trophy: level : ${level}
+  :bar_chart: Rank : ${userRank}
+  `
+          )
+          .setColor(embedColor)
+          .setFooter(message.guild.name)
+          .setTimestamp();
+        message.channel.send(levelEmbed);
+      }
       //   message.reply(` Your Level is ${level} , and you have ${xp} xp`);
-      const rank = new canvacord.Rank()
-        .setAvatar(
-          message.author.displayAvatarURL({ dynamic: false, format: "png" })
-        )
-        .setCurrentXP(xp)
-        .setRequiredXP(level * level * 100)
-        .setStatus(message.author.presence.status)
-        .setProgressBar("#FFFFFF", "COLOR")
-        .setUsername(message.author.username)
-        .setDiscriminator(message.author.discriminator)
-        .setLevel(level)
-        .renderEmojis(true)
-        // .setOverlay("#00ffff", 0.9)
-        // .setBackground("IMAGE", image)
-        .setRank(1, "", false);
+      if (levelCommandMessage.toLocaleLowerCase() === "card") {
+        const rank = new canvacord.Rank()
+          .setAvatar(
+            message.author.displayAvatarURL({ dynamic: false, format: "png" })
+          )
+          .setCurrentXP(xp)
+          .setRequiredXP(level * level * 100)
+          .setStatus(message.author.presence.status)
+          .setProgressBar("#FFFFFF", "COLOR")
+          .setUsername(message.author.username)
+          .setDiscriminator(message.author.discriminator)
+          .setLevel(level)
+          .renderEmojis(true)
+          // .setOverlay("#00ffff", 0.9)
+          // .setBackground("IMAGE", image)
+          .setRank(userRank, "", true);
 
-      rank.build().then((data) => {
-        const attachment = new Discord.MessageAttachment(data, "RankCard.png");
-        message.channel.send(attachment);
-      });
+        rank.build().then((data) => {
+          const attachment = new Discord.MessageAttachment(
+            data,
+            "RankCard.png"
+          );
+          message.channel.send(attachment);
+        });
+        return;
+      }
     });
   },
 };
